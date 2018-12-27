6ef400815979faa94b207cd150526a2f582cd3b5
Chaostheorie <cathero.official@gmail.com>
2018-12-27 11:34:31 +0100

    fix debug

--- main.py
+++ main.py
@@ -61,4 +61,4 @@ def new_snippet():
     return render_template('new_snippet.html', types=all_types)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
